ae85c051767b527d77d3c7522410c9d83edcfa30
Arturs Aboltins <arthuraboltins@gmail.com>
2023-02-20 22:39:02 +0000

    create questions

--- index.js
+++ index.js
@@ -5,17 +5,61 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'The title of my project: ',
+            name: 'title',
+        },
+        {
+            type: "list",
+            message: "License:",
+            name: "license",
+            choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License",  "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License"]
+        },
+        {
+            type: 'input',
+            message: 'Installation:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'usage:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Contributing:',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'Tests. To run tests, run the following command:​ ',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'questions:',
+            name: 'questions',
+        }
+    ])
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(readme, data) {
+//     fs.writeFile(readme, JSON.stringify(response, null, `\t`), (err) => {
+//         err ? console.log(err) : console.log("Success");
+//     });
+// }
 
-// function to initialize program
-function init() {
+// // writeToFile();
 
-}
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
